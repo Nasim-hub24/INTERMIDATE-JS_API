c47c5b2da1bbc3110ea3b6bef500b8bb7cacfdf1
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-06-03 21:23:32 +0600

    JSON in JavaScript

---
+++ JSON-DataLoad-DynamicWebsite/JSON.js
@@ -0,0 +1,38 @@
+/* 
+    ---------< JSON >--------
+
+    JSON stands for JavaScript Object Notation.
+    It is a format used to store and exchange data between applications, servers, and APIs.
+
+    ---------< OBJECT >--------
+
+    An object is a collection of related data stored as key-value pairs.
+
+    ---------< DIFFERENCE BETWEEN JSON AND OBJECT >-----------
+
+        | JavaScript Object             | JSON                              
+    | ------------------------------ | ----------------------------------    |
+    |. Used inside JavaScript code    | . Used for storing/transferring data |
+    |. Keys can be without quotes     | . Keys must be in double quotes      |
+    |. Can contain functions          | . Cannot contain functions           |
+    |. Variable can store it directly | . Usually stored as text/string      |
+
+*/
+
+
+const person = {
+    name : 'korim',
+    age : 30,
+    food : 'bef',
+    fruit : 'mango',
+    friends : ['solim', 'rahim', 'rohim', 'uddin']
+};
+console.log(person, typeof person);
+
+//     -------< OBJECT FORMAT TO JSON FORMAT >---------
+const personJson = JSON.stringify(person);
+console.log(personJson , typeof personJson);
+
+//      -------< STRING TO OBJECT >---------
+const parseJson = JSON.parse(personJson);
+console.log(parseJson);
