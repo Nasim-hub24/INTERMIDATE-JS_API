7841a5f0db434e135ec83a01bc83973f476be8d2
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-06-04 00:48:27 +0600

    access data using FETCH function

---
+++ JSON-DataLoad-DynamicWebsite/fetch.js
@@ -0,0 +1,60 @@
+
+
+/* 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+*/
+
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(Response => Response.json())
+        .then(json => console.log(json))
+}
+
+/*
+    PRACTICE 
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => console.log(data))
+*/
+
+/* 
+    --------------------[ FETCH EXPLANATION ]---------------
+
+    fetch() is used to get data from somewhere else (usually a server or API).
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+
+    This line means:
+    "Go to this URL and bring me the users data."
+
+    ----------< Why do we need it? >---------
+    Suppose you open Facebook or YouTube.
+
+    When the page loads, it needs:
+    > User information
+    > Posts
+    > Videos
+    > Comments
+
+    JavaScript uses fetch() to request that data from the server.
+
+    -----< Step 1 >--------
+    fetch(...)
+    Request data.
+
+    ------< Step 2 >-------
+    .then(response => response.json())
+
+    The server sends data back.
+    But JavaScript can't read it directly.
+    So we convert it into a JavaScript object using:
+    response.json()
+
+    -------< Step 3 >-------
+    .then(data => console.log(data))
+
+    Now we can use the data.
+*/
